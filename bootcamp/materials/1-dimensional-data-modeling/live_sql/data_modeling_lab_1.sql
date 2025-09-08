db440b7ad7338add3d158c68f9b0a91728207213
Merhawi <45622655+mtm-007@users.noreply.github.com>
2025-09-08 23:40:17 +0000

    cummulative data modeling with out shuffle needed infinitly paralizable query

--- bootcamp/materials/1-dimensional-data-modeling/live_sql/data_modeling_lab_1.sql
+++ bootcamp/materials/1-dimensional-data-modeling/live_sql/data_modeling_lab_1.sql
@@ -7,6 +7,9 @@
 -- 						reb REAL,
 -- 						ast REAL
 -- 					)
+-- DROP TABLE players
+
+-- CREATE TYPE scoring_class AS ENUM('star', 'good', 'average', 'bad');
 
 -- CREATE TABLE players (
 -- 	player_name TEXT,
@@ -17,6 +20,8 @@
 -- 	draft_round TEXT,
 -- 	draft_number TEXT,
 -- 	season_stats season_stats[],
+-- 	scoring_class scoring_class,
+-- 	years_since_last_season INTEGER,
 -- 	current_season INTEGER,
 -- 	PRIMARY KEY(player_name, current_season)
 -- )
@@ -57,24 +62,49 @@ SELECT
 					 )::season_stats]
 		ELSE y.season_stats
 		END AS season_stats,
+		CASE 
+			WHEN t.season IS NOT NULL THEN 
+				CASE WHEN t.pts > 20 THEN 'star'
+					 WHEN t.pts > 15 THEN 'good'
+					 WHEN t.pts > 10 THEN 'average'
+					 ELSE 'bad'
+				END::scoring_class
+				ELSE y.scoring_class
+			END AS scoring_class,
+		CASE
+			WHEN t.season IS NOT NULL THEN 0
+			ELSE y.years_since_last_season +1
+				END AS years_since_last_season,
 		COALESCE(t.season, y.current_season + 1) AS current_season
 		
 FROM today t FULL OUTER JOIN yesterday y
 		ON t.player_name= y.player_name
 
-SELECT * FROM players WHERE current_season = 2001
-AND player_name = 'Michael Jordan';
+
+-- infinility paralizeble fast with out the need for shuffle, done in the map stage with spark
+SELECT 	
+	player_name,
+	(season_stats[CARDINALITY(season_stats)]::season_stats).pts/
+	CASE WHEN (season_stats[1]::season_stats).pts=0 THEN 1 ELSE (season_stats[1]::season_stats).pts END
+	 AS latest_season
+FROM players 
+WHERE current_season = 2001
+AND scoring_class = 'star' 
+ORDER BY 2 DESC
+
+
+-- AND player_name = 'Michael Jordan';
 
 
 -- unnesting the season_stats array to the original player_seasons table
-WITH unnested AS(
-	SELECT player_name,
-			UNNEST(season_stats)::season_stats AS season_stats
-	FROM players
-	WHERE current_season = 2001
-	AND player_name = 'Michael Jordan'
-)
+-- WITH unnested AS(
+-- 	SELECT player_name,
+-- 			UNNEST(season_stats)::season_stats AS season_stats
+-- 	FROM players
+-- 	WHERE current_season = 2001
+-- 	AND player_name = 'Michael Jordan'
+-- )
 
-SELECT player_name,
-		(season_stats::season_stats).*
-FROM unnested
+-- SELECT player_name,
+-- 		(season_stats::season_stats).*
+-- FROM unnested
