34286f75525692099f962d363fca52cd29f38108
Merhawi <45622655+mtm-007@users.noreply.github.com>
2025-09-08 08:57:29 +0000

    add persistent scripts to pgadmin in docker-compose.yml

--- bootcamp/materials/1-dimensional-data-modeling/live_sql/data_modeling_lab_1.sql
+++ bootcamp/materials/1-dimensional-data-modeling/live_sql/data_modeling_lab_1.sql
@@ -8,15 +8,59 @@
 -- 						ast REAL
 -- 					)
 
-CREATE TABLE players (
-	player_name TEXT,
-	height TEXT,
-	college TEXT,
-	country TEXT,
-	draft_year TEXT,
-	draft_round TEXT,
-	draft_number TEXT,
-	season_stats season_stats[],
-	current_season INTEGER,
-	PRIMARY KEY(player_name, current_season)
-)
+-- CREATE TABLE players (
+-- 	player_name TEXT,
+-- 	height TEXT,
+-- 	college TEXT,
+-- 	country TEXT,
+-- 	draft_year TEXT,
+-- 	draft_round TEXT,
+-- 	draft_number TEXT,
+-- 	season_stats season_stats[],
+-- 	current_season INTEGER,
+-- 	PRIMARY KEY(player_name, current_season)
+-- )
+
+-- seed query for cumulative data modeling
+
+INSERT INTO players
+WITH yesterday AS(
+	SELECT * FROM players
+	WHERE current_season = 2000 --changing the years adding up
+),
+	today AS(
+		SELECT * FROM player_seasons
+		WHERE season = 2001
+	)
+SELECT 
+		COALESCE(t.player_name, y.player_name) AS player_name,
+		COALESCE(t.height, y.height) AS height,
+		COALESCE(t.college, y.college) AS college,
+		COALESCE(t.country, y.country) AS country,
+		COALESCE(t.draft_year, y.draft_year) AS draft_year,
+		COALESCE(t.draft_round, y.draft_round) AS draft_round,
+		COALESCE(t.draft_number, y.draft_number) AS draft_number,
+		CASE WHEN y.season_stats IS NULL
+			THEN ARRAY[ROW(
+					t.season,
+					t.gp,
+					t.pts,
+					t.reb,
+					t.ast
+					 )::season_stats]
+		WHEN t.season IS NOT NULL THEN y.season_stats || ARRAY[ROW(
+					t.season,
+					t.gp,
+					t.pts,
+					t.reb,
+					t.ast
+					 )::season_stats]
+		ELSE y.season_stats
+		END AS season_stats,
+		COALESCE(t.season, y.current_season + 1) AS current_season
+		
+FROM today t FULL OUTER JOIN yesterday y
+		ON t.player_name= y.player_name
+
+SELECT * FROM players WHERE current_season = 2001
+AND player_name = 'Michael Jordan';
